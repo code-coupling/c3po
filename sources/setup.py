ca5cce58880a637375026b91bcb84f9ec196104f
Cyril PATRICOT - DEN/DANS/DM2S/SERMA/LPEC - 2012/10/04 <cpatricot@is233268.intra.cea.fr>
2021-02-15 16:28:45 +0100

    Fichier setup.py deplace dans sources/ et complete.

---
+++ sources/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name = "C3PO",
+      version = "1.0",
+      author = "CEA",
+      author_email = "cyril.patricot@cea.fr",
+      maintainer = "Cyril Patricot",
+      maintainer_email = "cyril.patricot@cea.fr",
+      url = "https://sourceforge.net/projects/cea-c3po/",
+      packages = ["C3PO", "C3POMPI"],
+      description = "Collaborative Code Coupling PlatfOrm",
+      licence = "3-Clause BSD"
+      )
+
