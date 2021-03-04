37aafd0136abad63e48ed24bdf0026b6998571eb
Matthieu LEMAIRE - DES/ISAS/DM2S/SERMA/LPEC - 2020/09/28 <ml234078@is243233.intra.cea.fr>
2021-03-04 18:12:52 +0100

    Mise à jour majeure de CRONOS2Driver.py

--- sources/C3PO/physicsDrivers/CRONOS2Driver.py
+++ sources/C3PO/physicsDrivers/CRONOS2Driver.py
@@ -28,21 +28,20 @@ class CRONOS2Driver(PhysicsDriver):
         PhysicsDriver.__init__(self)
         self.isInit_ = False
 
-        # jeu de donnees CRONOS2
+        # CRONOS2 input file
         self.datafile_ = ""
-        # session Access python
+
+        # Access session in python
         self.a_ = 0
 
-        # champs MED
-        self.MEDTECO_ = 0
-        self.MEDDMOD_ = 0
-        self.MEDPUIS_ = 0
-
-        # nom des champs MED
+        # name of MED fields
         self.tag_ = TagClass() 
 
-        # temps physique
+        # physical time
         self.t_ = 0
+
+        # time step for transient
+        self.dt_ = 0
 
     def set_tag(self, tag):
         self.tag_ = tag
@@ -57,67 +56,76 @@ class CRONOS2Driver(PhysicsDriver):
             self.isInit = True
             if bool(self.datafile_):
                 self.a_.evalFile(self.datafile_)
-            self.a_.eval("T_C3PO T_RES T_STR T_OPT = C3PO_INITIALIZE T_IMP T_STR T_OPT T_RES ;") 
+            self.a_.eval("T_C3PO = TABLE: ; T_C3PO.'ITH' = 0 ; T_C3PO.'MED' = TABLE: ;")
+            self.a_.eval("T_C3PO T_RES T_STR T_OPT = ICOCO_INITIALIZE T_IMP T_STR T_OPT T_RES T_C3PO ;") 
         return True
 
     def terminate(self):
-        self.a_.eval("C3PO_TERMINATE T_IMP T_STR T_OPT T_RES T_C3PO ;") 
+        self.a_.eval("ICOCO_TERMINATE T_IMP T_STR T_OPT T_RES T_C3PO ;") 
         self.a_.eval("EDTIME: 'TOUT' ; MEMOIRE: -1 ; ARRET: ;")
         self.a_.end()        
         self.isInit_ = False
         return True
 
     def initTimeStep(self, dt):
+        self.dt_ = dt
         self.a_.eval("T_C3PO.'DT' = "+"{:.3f}".format(dt)+" ;")
-        self.a_.eval("T_C3PO T_RES T_STR T_OPT = C3PO_INIT_TIME_STEP T_IMP T_STR T_OPT T_RES T_C3PO ;") 
+        self.a_.eval("T_C3PO T_RES T_STR T_OPT = ICOCO_INIT_TIME_STEP T_IMP T_STR T_OPT T_RES T_C3PO ;") 
         return True
 
     def presentTime(self):
         return self.t_ 
 
     def computeTimeStep(self):
+        """ Description of the expected outputs for the ICOCO_COMPUTE_TIME_STEP gibiane subroutine """
+        """ The time step (floating-point number) must be output in the variable T_C3PO.'DT' """
+        """ The stop flag must be output as an integer in the variable T_C3PO.'STOP' """
+        """ (because 'getTableBool' sadly does not exist in Access so 'getTableInt' is used instead) """
+        """ T_C3PO.'STOP'==0 means FALSE (do not stop) and T_C3PO.'STOP'>0 means TRUE (please stop)"""
+
         self.a_.eval("T_C3PO.'PRESENT_TIME' = "+"{:.3f}".format(self.t_)+" ;")
-        self.a_.eval("T_C3PO T_RES T_STR T_OPT = C3PO_COMPUTE_TIME_STEP T_IMP T_STR T_OPT T_RES T_C3PO ;") 
-        tcal_ptr = self.a_.getTabPtr("T_C3PO")
-        dt = self.a_.getTableFloat(tcal_ptr,"DT")
-        stop = bool(self.a_.getTableFloat(tcal_ptr,"STOP"))
+        self.a_.eval("T_C3PO T_RES T_STR T_OPT = ICOCO_COMPUTE_TIME_STEP T_IMP T_STR T_OPT T_RES T_C3PO ;") 
+        ptr = self.a_.getTabPtr("T_C3PO")
+        dt = self.a_.getTableFloat(ptr,"DT")
+        stop = bool(self.a_.getTableInt(ptr,"STOP"))
         return (dt, stop)
 
     def solveTimeStep(self):
-        self.a_.eval("T_C3PO T_RES T_STR T_OPT = C3PO_SOLVE_TIME_STEP T_IMP T_STR T_OPT T_RES T_C3PO ;") 
+        self.a_.eval("T_C3PO T_RES T_STR T_OPT = ICOCO_SOLVE_TIME_STEP T_IMP T_STR T_OPT T_RES T_C3PO ;") 
         return True
 
     def validateTimeStep(self):
-        tcal_ptr = self.a_.getTabPtr("T_C3PO")
-        dt = self.a_.getTableFloat(tcal_ptr,"DT")
-        self.t_ = self.t_ + dt 
+        self.t_ = self.t_ + self.dt_ 
         self.a_.eval("T_C3PO.'PRESENT_TIME' = "+"{:.3f}".format(self.t_)+" ;")
-        self.a_.eval("T_C3PO T_RES T_STR T_OPT = C3PO_VALIDATE_TIME_STEP T_IMP T_STR T_OPT T_RES T_C3PO ;") 
+        self.a_.eval("T_C3PO T_RES T_STR T_OPT = ICOCO_VALIDATE_TIME_STEP T_IMP T_STR T_OPT T_RES T_C3PO ;") 
 
     def abortTimeStep(self):
-        pass
+        if self.dt_ > 0:
+            raise Exception("CRONOS2Driver.abortTimeStep: NotImplemented.")
+        else:
+            pass
 
     def getOutputMEDField(self, name):
         if name == self.tag_.PUIS_:
-            self.a_.eval("GR__ = CGRILLE: 0 T_STR.'DOMAINE' ; MESH_sortie = MED_GRID_2_MESH: GR__ ; ")  
-            self.a_.eval("pcoeurMW = T_OPT.'PUIS_RELATIVE' * T_OPT.'MW' ; ")     
-            self.a_.eval("PUIS_MW FLUX_ncm2s = NORMALISATION: 0 'PUISSANCE COEUR' pcoeurMW T_STR.'FLUXINT' T_STR.'PUIS' T_STR.'DOMAINE' ; ")          
-            self.a_.eval("GRILLE_sortie = CGRILLE: 0 PUIS_MW T_STR.'DOMAINE' ;      ")      
-            self.a_.eval("GRILLE_sortie = FONCTION_GRILLE: 0 GRILLE_sortie 'PUISSANCE INTEGREE' 'FINAL' 'PUISSANCE_W' ; ")         
-            self.a_.eval("GRILLE_sortie = 1.0E6 * GRILLE_sortie; ")      
-            self.a_.eval("FIELD_sortie = MED_GRID_2_FIELD: GRILLE_sortie MESH_sortie ; ")
+            self.a_.eval("GR__ = CGRILLE: 0 T_STR.'DOMAINE' ; MESH_sortie = MED_GRID_2_MESH: GR__ ;")  
+            self.a_.eval("pcoeurMW = T_OPT.'PUIS_RELATIVE' * T_OPT.'MW' ;")     
+            self.a_.eval("PUIS_MW FLUX_ncm2s = NORMALISATION: 0 'PUISSANCE COEUR' pcoeurMW T_STR.'FLUXINT' T_STR.'PUIS' T_STR.'DOMAINE' ;")          
+            self.a_.eval("GRILLE_sortie = CGRILLE: 0 PUIS_MW T_STR.'DOMAINE' ;")      
+            self.a_.eval("GRILLE_sortie = FONCTION_GRILLE: 0 GRILLE_sortie 'PUISSANCE INTEGREE' 'FINAL' 'PUISSANCE_W' ;")         
+            self.a_.eval("GRILLE_sortie = 1.0E6 * GRILLE_sortie ;")      
+            self.a_.eval("FIELD_sortie = MED_GRID_2_FIELD: GRILLE_sortie MESH_sortie ;")
             myCppPtr = self.a_.getCppPtr("FIELD_sortie")
-            self.MEDPUIS_ = MEDconvert.void2field(myCppPtr)
-            self.MEDPUIS_.setNature(MEDCoupling.Integral)
-            return self.MEDPUIS_
+            field_output = MEDconvert.void2field(myCppPtr)
+            field_output.setNature(MEDCoupling.Integral)
+            return field_output
         else:
             raise Exception("CRONOS2Driver.getOutputMEDField Only " + self.tag_.PUIS_ + " output available.")
 
     def getInputMEDFieldTemplate(self, name):
         if ((name == self.tag_.TECO_) or (name == self.tag_.DMOD_)):
-            self.a_.eval("GR__ = CGRILLE: 0 T_STR.'DOMAINE' ; MESH_sortie = MED_GRID_2_MESH: GR__ ; ")  
+            self.a_.eval("GR__ = CGRILLE: 0 T_STR.'DOMAINE' ; MESH_sortie = MED_GRID_2_MESH: GR__ ;")  
             self.a_.eval("name = '"+str(name)+"' ;")
-            self.a_.eval("GRILLE_sortie = CGRILLE: 0 T_STR.'TCOMP' T_STR.'DOMAINE' name;")
+            self.a_.eval("GRILLE_sortie = CGRILLE: 0 T_STR.'TCOMP' T_STR.'DOMAINE' name ;")
             self.a_.eval("FIELD_sortie = MED_GRID_2_FIELD: GRILLE_sortie MESH_sortie ;")
             myCppPtr = self.a_.getCppPtr("FIELD_sortie")
             field_template = MEDconvert.void2field(myCppPtr)
@@ -126,20 +134,14 @@ class CRONOS2Driver(PhysicsDriver):
             raise Exception("CRONOS2Driver.getIntputMEDFieldTemplate Only " + self.tag_.TECO_ + " and " + self.tag_.DMOD_ + " template available.")
  
     def setInputMEDField(self, name, field):
-        self.a_.eval("grille_ref = CGRILLE: 0 T_STR.'TCOMP' T_STR.'DOMAINE' ;")
-        self.a_.eval("ITH = T_C3PO.'ITH' ;")
-        self.a_.eval("name = '"+str(name)+"' ;")
-        intField = MEDtsetpt.SaphGetIdFromPtr(field) 
-        if name == self.tag_.TECO_:
-            self.a_.eval("T_C3PO.'TECOTAB'.ITH = MED_FIELD_SAPH: "+str(intField)+" ;") 
-            self.a_.eval("grille_teco = MED_FIELD_2_GRID: grille_ref T_C3PO.'TECOTAB'.ITH ;")
-            self.a_.eval("T_STR.'TCOMP' = CGRILLE2C: 0 T_STR.'TCOMP' T_STR.'DOMAINE' grille_teco name ; ")
-            self.MEDTECO_ = field
-        elif name == self.tag_.DMOD_:
-            self.a_.eval("T_C3PO.'DMODTAB'.ITH = MED_FIELD_SAPH: "+str(intField)+" ;") 
-            self.a_.eval("grille_dmod = MED_FIELD_2_GRID: grille_ref T_C3PO.'DMODTAB'.ITH ;")
-            self.a_.eval("T_STR.'TCOMP' = CGRILLE2C: 0 T_STR.'TCOMP' T_STR.'DOMAINE' grille_dmod name ; ")
-            self.MEDDMOD_ = field
+        if (name == self.tag_.TECO_ or name == self.tag_.DMOD_):
+            self.a_.eval("T_C3PO.'ITH' = T_C3PO.'ITH' + 1 ; ITH = T_C3PO.'ITH' ;")
+            intField = MEDtsetpt.SaphGetIdFromPtr(field) 
+            self.a_.eval("T_C3PO.'MED'.ITH = MED_FIELD_SAPH: "+str(intField)+" ;") 
+            self.a_.eval("GRILLE_ref = CGRILLE: 0 T_STR.'TCOMP' T_STR.'DOMAINE' ;")
+            self.a_.eval("GRILLE_sortie = MED_FIELD_2_GRID: GRILLE_ref T_C3PO.'MED'.ITH ;")
+            self.a_.eval("name = '"+str(name)+"' ;")
+            self.a_.eval("T_STR.'TCOMP' = CGRILLE2C: 0 T_STR.'TCOMP' T_STR.'DOMAINE' GRILLE_sortie name ;")
         else:
             raise Exception("CRONOS2Driver.setInputMEDField Only " + self.tag_.TECO_ + " and " + self.tag_.DMOD_ + " input possible.")
  
