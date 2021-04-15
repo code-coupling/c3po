39a52f66e2cbc7d406e72400aa3e80d03fd06283
abn <adrien.bruneton@cea.fr>
2021-04-15 21:30:47 +0200

    [C3PO -> c3po] Moving C3PO directory into c3po and C3POMPI into c3po/mpi

--- sources/setup.py
+++ sources/setup.py
@@ -8,21 +8,21 @@
 # 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-""" Allow pip to install C3PO. Once installed, C3PO is known by Python without need to set the PYTHONPATH environment variable.
+""" Allow pip to install c3po. Once installed, c3po is known by Python without need to set the PYTHONPATH environment variable.
 
 Run "pip install <path>" (or "pip install --user <path>") to do so, with <path> the path to the folder containing this setup.py file.
 """
 
 from setuptools import setup
 
-setup(name="C3PO",
+setup(name="c3po",
       version="1.1",
       author="CEA",
       author_email="cyril.patricot@cea.fr",
       maintainer="Cyril Patricot",
       maintainer_email="cyril.patricot@cea.fr",
       url="https://sourceforge.net/projects/cea-c3po/",
-      packages=["C3PO", "C3POMPI"],
+      packages=["c3po"],
       description="Collaborative Code Coupling PlatfOrm",
       licence="3-Clause BSD"
       )
