d037f2dab04c6f72d54e7e1750114db2536806b2
Quentin Lancereau <quentin.lancereau@cea.fr>
2021-05-06 13:43:55 +0200

    Correction d'un bug dans setup.py

--- sources/setup.py
+++ sources/setup.py
@@ -22,7 +22,11 @@ setup(name="C3PO",
       maintainer="Cyril Patricot",
       maintainer_email="cyril.patricot@cea.fr",
       url="https://sourceforge.net/projects/cea-c3po/",
-      packages=["C3PO", "C3POMPI"],
+      packages=["C3PO",
+                "C3PO.couplers",
+                "C3PO.exchangeMethods",
+                "C3PO.physicsDrivers",
+                "C3POMPI"],
       description="Collaborative Code Coupling PlatfOrm",
       licence="3-Clause BSD"
       )
