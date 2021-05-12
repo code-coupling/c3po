6c71925c3bd8d5616da89e82d40b359ba89d96a2
Cyril PATRICOT - DEN/DANS/DM2S/SERMA/LPEC - 2012/10/04 <cpatricot@is233268.intra.cea.fr>
2021-05-12 16:36:13 +0200

    Standardisation des noms de physicsDrivers : AP3Driver -> APOLLO3Driver, Alcyone2Driver -> ALCYONE2Driver et Cathare3Driver -> CATHARE3Driver.
    Modifications faites avec des find . -name "*.py" | xargs sed -i "s/nom1/nom2/g"

--- sources/c3po/physicsDrivers/CATHARE3Driver.py
+++ sources/c3po/physicsDrivers/CATHARE3Driver.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
+
+# Copyright (c) 2020, CEA
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+""" Contain the class CATHARE3Driver. """
 from __future__ import print_function, division
 
 import c3po.medcoupling_compat as mc
@@ -21,7 +31,7 @@ def build_name(keyword, cname, loc, irad=-1):
     return new_name
 
 
-class PBC(C3, PhysicsDriver):
+class CATHARE3Driver(C3, PhysicsDriver):
     """! This is the implementation of PhysicsDriver for CATHARE3. """
 
     def __init__(self):
