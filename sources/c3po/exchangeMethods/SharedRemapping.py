717097c6bc533698ddae73422f34f0600f6246a7
Cyril PATRICOT - DEN/DANS/DM2S/SERMA/LPEC - 2012/10/04 <cpatricot@is233268.intra.cea.fr>
2021-05-21 15:27:19 +0200

    SharedRemapping (et SharedRemappingMulti1D3D) : l'argument outsideCellsScreening passe aussi a l'objet Remapper. SharedRemappingMulti1D3D utilise maintenant SharedRemapping pour la projection (sans impact utilisateur mais evite la duplication de code).

--- sources/c3po/exchangeMethods/SharedRemapping.py
+++ sources/c3po/exchangeMethods/SharedRemapping.py
@@ -18,7 +18,7 @@ from c3po.exchangeMethods.ExchangeMethod import ExchangeMethod
 class Remapper(MEDCouplingRemapper):
     """! Allow to share the mesh projection for different SharedRemapping objects by building them with the same instance of this class. """
 
-    def __init__(self, meshAlignment=False, offset=[0., 0., 0.], rescaling=1., rotation=0.):
+    def __init__(self, meshAlignment=False, offset=[0., 0., 0.], rescaling=1., rotation=0., outsideCellsScreening=False):
         """! Build a Remapper object.
 
         @param meshAlignment If set to True, at the initialization phase of the Remapper object, meshes are translated such as their "bounding
@@ -31,6 +31,10 @@ class Remapper(MEDCouplingRemapper):
         @param rotation Value of the rotation between the source and the target meshes. The rotation is centered on [0., 0., 0.] and is about
             the vertical axis. >0 means that the source mesh is rotated of the given angle compared to the target one. The inverse rotation is
             applied to the source mesh, after mesh alignment or translation, if any. pi means half turn.
+        @param outsideCellsScreening If set to True, target cells whose barycentre is outside of source mesh are screen out (defaultValue is
+               assigned to them). It can be useful to screen out cells that are in contact with the source mesh, but that should not be intersected
+               by it. On the other hand, it will screen out cells actually intersected if their barycenter is outside of source mesh !
+               Be careful with this option.
         """
         MEDCouplingRemapper.__init__(self)
         self.isInit = False
@@ -40,6 +44,9 @@ class Remapper(MEDCouplingRemapper):
             raise Exception("Remapper: rescaling must be > 0!")
         self._rescaling = rescaling
         self._rotation = rotation
+        self._outsideCellsScreening = outsideCellsScreening
+        self._cellsToScreenOutSource = []
+        self._cellsToScreenOutTarget = []
 
     def initialize(self, sourceMesh, targetMesh):
         """! INTERNAL """
@@ -55,7 +62,36 @@ class Remapper(MEDCouplingRemapper):
         if self._rotation != 0.:
             sourceMesh.rotate([0., 0., 0.], [0., 0., 1.], self._rotation)
         self.prepare(sourceMesh, targetMesh, "P0P0")
+        if self._outsideCellsScreening:
+            self._cellsToScreenOutTarget = self.computeCellsToScreenOut(targetMesh, sourceMesh)
+            self._cellsToScreenOutSource = self.computeCellsToScreenOut(sourceMesh, targetMesh)
         self.isInit = True
+
+    def computeCellsToScreenOut(self, mesh1, mesh2):
+        """! INTERNAL """
+        bary = []
+        try:
+            bary = mesh1.computeCellCenterOfMass()  # MEDCoupling 9
+        except:
+            bary = mesh1.getBarycenterAndOwner()  # MEDCoupling 7
+        _, cellsId = mesh2.getCellsContainingPoints(bary, 1.0e-8)
+        dsi = cellsId.deltaShiftIndex()
+        try:
+            return dsi.findIdsEqual(0)  # MEDCoupling 9
+        except:
+            return dsi.getIdsEqual(0)  # MEDCoupling 7
+
+    def directRemap(self, field, defaultValue):
+        """! INTERNAL """
+        outputField = MEDCouplingRemapper.transferField(self, field, defaultValue)
+        outputField.getArray()[self._cellsToScreenOutTarget] = defaultValue
+        return outputField
+
+    def reverseRemap(self, field, defaultValue):
+        """! INTERNAL """
+        outputField = MEDCouplingRemapper.reverseTransferField(self, field, defaultValue)
+        outputField.getArray()[self._cellsToScreenOutSource] = defaultValue
+        return outputField
 
 
 class SharedRemapping(ExchangeMethod):
@@ -74,7 +110,7 @@ class SharedRemapping(ExchangeMethod):
     SharedRemapping through a Remapper object.
     """
 
-    def __init__(self, remapper, reverse=False, defaultValue=0., linearTransform=(1., 0.), outsideCellsScreening=False):
+    def __init__(self, remapper, reverse=False, defaultValue=0., linearTransform=(1., 0.)):
         """! Build an SharedRemapping object, to be given to an Exchanger.
 
         @param remapper A Remapper object (defined in C3PO and inheriting from MEDCouplingRemapper) performing the projection. It
@@ -85,55 +121,32 @@ class SharedRemapping(ExchangeMethod):
                intersected by the source mesh.
         @param linearTransform Tuple (a,b): apply a linear function to all output fields f such as they become a * f + b. The transformation
                is applied after the mesh projection.
-        @param outsideCellsScreening If set to True, target cells whose barycentre is outside of source mesh are screen out (defaultValue is
-               assigned to them). It can be useful to screen out cells that are in contact with the source mesh, but that should not be intersected
-               by it. On the other hand, it will screen out cells actually intersected if their barycenter is outside of source mesh !
-               Be careful with this option.
         """
         self._remapper = remapper
         self._isReverse = reverse
         self._defaultValue = defaultValue
         self._linearTransform = linearTransform
-        self._outsideCellsScreening = outsideCellsScreening
-        self._cellIdsToScreenOut = []
-        self._isCellScreeningInit = False
 
-    def initialize(self, fieldsToGet, fieldsToSet, valuesToGet):
+    def initialize(self, fieldsToGet, fieldsToSet):
         """! INTERNAL """
         if not self._remapper.isInit:
             if self._isReverse:
                 self._remapper.initialize(fieldsToSet[0].getMesh(), fieldsToGet[0].getMesh())
             else:
                 self._remapper.initialize(fieldsToGet[0].getMesh(), fieldsToSet[0].getMesh())
-        if self._outsideCellsScreening and not self._isCellScreeningInit:
-            bary = []
-            try:
-                bary = fieldsToSet[0].getMesh().computeCellCenterOfMass()  # MEDCoupling 9
-            except:
-                bary = fieldsToSet[0].getMesh().getBarycenterAndOwner()  # MEDCoupling 7
-            _, cellsId = fieldsToGet[0].getMesh().getCellsContainingPoints(bary, 1.0e-8)
-            dsi = cellsId.deltaShiftIndex()
-            try:
-                self._cellIdsToScreenOut = dsi.findIdsEqual(0)  # MEDCoupling 9
-            except:
-                self._cellIdsToScreenOut = dsi.getIdsEqual(0)  # MEDCoupling 7
-            self._isCellScreeningInit = True
 
     def __call__(self, fieldsToGet, fieldsToSet, valuesToGet):
         """! Project the input fields one by one before returning them as outputs, in the same order. """
         if len(fieldsToSet) != len(fieldsToGet):
             raise Exception("sharedRemapping : there must be the same number of input and output MED fields")
 
-        self.initialize(fieldsToGet, fieldsToSet, valuesToGet)
+        self.initialize(fieldsToGet, fieldsToSet)
         transformedMED = []
         for i in range(len(fieldsToSet)):
             if self._isReverse:
-                transformedMED.append(self._remapper.reverseTransferField(fieldsToGet[i], self._defaultValue))
+                transformedMED.append(self._remapper.reverseRemap(fieldsToGet[i], self._defaultValue))
             else:
-                transformedMED.append(self._remapper.transferField(fieldsToGet[i], self._defaultValue))
-        if self._outsideCellsScreening:
-            for med in transformedMED:
-                med.getArray()[self._cellIdsToScreenOut] = self._defaultValue
+                transformedMED.append(self._remapper.directRemap(fieldsToGet[i], self._defaultValue))
         if self._linearTransform != (1., 0.):
             for med in transformedMED:
                 med.applyLin(*(self._linearTransform))
--- sources/c3po/exchangeMethods/SharedRemappingMulti1D3D.py
+++ sources/c3po/exchangeMethods/SharedRemappingMulti1D3D.py
@@ -12,14 +12,13 @@
 from __future__ import print_function, division
 
 import c3po.medcouplingCompat as mc
-from c3po.medcouplingCompat import MEDCouplingRemapper
-from c3po.exchangeMethods.ExchangeMethod import ExchangeMethod
+from c3po.exchangeMethods.SharedRemapping import Remapper, SharedRemapping
 
 
-class Multi1D3DRemapper(MEDCouplingRemapper):
+class Multi1D3DRemapper(Remapper):
     """! Allow to share the mesh projection for different SharedRemappingMulti1D3D objects by building them with the same instance of this class. """
 
-    def __init__(self, xCoordinates, yCoordinates, indexTable, weights, meshAlignment=False, offset=[0., 0., 0.], rescaling=1., rotation=0.):
+    def __init__(self, xCoordinates, yCoordinates, indexTable, weights, meshAlignment=False, offset=[0., 0., 0.], rescaling=1., rotation=0., outsideCellsScreening=False):
         """! Build a Multi1D3DRemapper object.
 
         An intermediate inner 3D mesh is built from a 2D grid defined by the parameters.
@@ -33,19 +32,13 @@ class Multi1D3DRemapper(MEDCouplingRemapper):
         @param indexTable For each position of the 2D grid (x coordinate changes first), the index of the 1D field to associate. Put -1 to
         associate to nothing.
         @param weights Weigh of each 1D field to take into account for extensive variables.
-        @param meshAlignment If set to True, at the initialization phase of the remapper object, meshes are translated such as their "bounding box"
-               are radially centred on (x = 0., y = 0.) and have zmin = 0.
-        @param offset Value of the 3D offset between the source (multi1D) and the target (3D) meshes (>0 on z means that the source mesh is above the
-               target one). The given vector is used to translate the source mesh (after the mesh alignment, if any).
-        @param rescaling Value of a rescaling factor to be applied between the source (multi1D) and the target (3D) meshes (>1 means that the source
-               mesh is initially larger than the target one). The scaling is centered on [0., 0., 0.] and is applied to the source mesh after mesh
-               alignment or translation, if any.
-        @param rotation Value of the rotation between the source (multi1D) and the target (3D) meshes. The rotation is centered on [0., 0., 0.] and
-            is about the vertical axis. >0 means that the source mesh is rotated of the given angle compared to the target one. The inverse rotation
-            is applied to the source mesh, after mesh alignment or translation, if any. pi means half turn.
+        @param meshAlignment see Remapper. The source mesh is the multi1D one and the target mesh the 3D one.
+        @param offset see Remapper. The source mesh is the multi1D one and the target mesh the 3D one.
+        @param rescaling see Remapper. The source mesh is the multi1D one and the target mesh the 3D one.
+        @param rotation see Remapper. The source mesh is the multi1D one and the target mesh the 3D one.
+        @param outsideCellsScreening see Remapper. The source mesh is the multi1D one and the target mesh the 3D one.
         """
-        MEDCouplingRemapper.__init__(self)
-
+        Remapper.__init__(self, meshAlignment, offset, rescaling, rotation, outsideCellsScreening)
         self._indexTable = [[] for k in range(max(indexTable) + 1)]
         for position, indice1D in enumerate(indexTable):
             if indice1D >= 0:
@@ -65,17 +58,9 @@ class Multi1D3DRemapper(MEDCouplingRemapper):
         self._innerMesh = mc.MEDCouplingCMesh("3DMeshFromMulti1D")
         self._innerField = mc.MEDCouplingFieldDouble(mc.ON_CELLS, mc.ONE_TIME)
         self._innerField.setName("3DFieldFromMulti1D")
-        self.isInit = False
-        self._meshAlignment = meshAlignment
-        self._offset = offset
-        if rescaling <= 0.:
-            raise Exception("Multi1D3DRemapper: rescaling must be > 0!")
-        self._rescaling = rescaling
-        self._rotation = rotation
-        self._cellsToScreenOut3DMesh = []
-        self._cellsToScreenOutInnerMesh = []
+        self.isInnerFieldBuilt = False
 
-    def initialize(self, mesh1D, mesh3D):
+    def buildInnerField(self, mesh1D):
         """! INTERNAL """
         self._arrayZ = mesh1D.getCoordsAt(0)
         self._innerMesh.setCoords(self._arrayX, self._arrayY, self._arrayZ)
@@ -85,66 +70,30 @@ class Multi1D3DRemapper(MEDCouplingRemapper):
         array.alloc(self._numberOfCellsIn1D * self._numberOf1DPositions)
         array.fillWithValue(0.)
         self._innerField.setArray(array)
-        if self._meshAlignment:
-            for mesh in [self._innerMesh, mesh3D]:
-                [(xmin, xmax), (ymin, ymax), (zmin, _)] = mesh.getBoundingBox()
-                offsettmp = [-0.5 * (xmin + xmax), -0.5 * (ymin + ymax), -zmin]
-                mesh.translate(offsettmp)
-        if self._offset != [0., 0., 0.]:
-            self._innerMesh.translate([-x for x in self._offset])
-        if self._rescaling != 1.:
-            self._innerMesh.scale([0., 0., 0.], 1. / self._rescaling)
-        if self._rotation != 0.:
-            self._innerMesh.rotate([0., 0., 0.], [0., 0., 1.], self._rotation)
-        self.prepare(self._innerMesh, mesh3D, "P0P0")
+        self.isInnerFieldBuilt = True
 
-        bary = []
-        try:
-            bary = self._innerMesh.computeCellCenterOfMass()  # MEDCoupling 9
-        except:
-            bary = self._innerMesh.getBarycenterAndOwner()  # MEDCoupling 7
-        _, cellsId = mesh3D.getCellsContainingPoints(bary, 1.0e-8)
-        dsi = cellsId.deltaShiftIndex()
-        try:
-            self._cellsToScreenOutInnerMesh = dsi.findIdsEqual(0)  # MEDCoupling 9
-        except:
-            self._cellsToScreenOutInnerMesh = dsi.getIdsEqual(0)  # MEDCoupling 7
-        try:
-            bary = mesh3D.computeCellCenterOfMass()
-        except:
-            bary = mesh3D.getBarycenterAndOwner()
-        _, cellsId = self._innerMesh.getCellsContainingPoints(bary, 1.0e-8)
-        dsi = cellsId.deltaShiftIndex()
-        try:
-            self._cellsToScreenOut3DMesh = dsi.findIdsEqual(0)
-        except:
-            self._cellsToScreenOut3DMesh = dsi.getIdsEqual(0)
-
-        self.isInit = True
-
-    def build3DField(self, fields1D, defaultValue, outsideCellsScreening):
+    def getInnerField(self):
         """! INTERNAL """
+        return self._innerField
+
+    def build3DField(self, fields1D):
+        """! INTERNAL """
+        resuField = self._innerField.clone(True)
         if len(fields1D) > 0:
-            self._innerField.setNature(fields1D[0].getNature())
-        array3D = self._innerField.getArray()
+            resuField.setNature(fields1D[0].getNature())
+        array3D = resuField.getArray()
         nbOfElems3D = array3D.getNbOfElems()
         for i, field in enumerate(fields1D):
             array1D = field.getArray()
-            if self._innerField.getNature() == mc.ExtensiveMaximum or self._innerField.getNature() == mc.ExtensiveConservation:
+            if resuField.getNature() == mc.ExtensiveMaximum or resuField.getNature() == mc.ExtensiveConservation:
                 array1D *= self._weights[i]
             for position in self._indexTable[i]:
                 array3D.setPartOfValues1(array1D, position, nbOfElems3D, self._numberOf1DPositions, 0, 1, 1)
-        resuField = self.transferField(self._innerField, defaultValue)
-        if outsideCellsScreening:
-            resuField.getArray()[self._cellsToScreenOut3DMesh] = defaultValue
         return resuField
 
-    def build1DFields(self, field3D, defaultValue, outsideCellsScreening):
+    def build1DFields(self, field3D):
         """! INTERNAL """
-        self._innerField = self.reverseTransferField(field3D, defaultValue)
-        array3D = self._innerField.getArray()
-        if outsideCellsScreening:
-            array3D[self._cellsToScreenOutInnerMesh] = defaultValue
+        array3D = field3D.getArray()
         fields1D = []
         for i, list1D in enumerate(self._indexTable):
             fields1D.append(mc.MEDCouplingFieldDouble(mc.ON_CELLS, mc.ONE_TIME))
@@ -168,7 +117,7 @@ class Multi1D3DRemapper(MEDCouplingRemapper):
         return fields1D
 
 
-class SharedRemappingMulti1D3D(ExchangeMethod):
+class SharedRemappingMulti1D3D(SharedRemapping):
     """! SharedRemappingMulti1D3D is an ExchangeMethod which projects the input fields one by one before returning them as
     outputs, in the same order.
 
@@ -186,50 +135,32 @@ class SharedRemappingMulti1D3D(ExchangeMethod):
     of SharedRemappingMulti1D3D.
     """
 
-    def __init__(self, remapper, reverse=False, defaultValue=0., linearTransform=(1., 0.), outsideCellsScreening=False):
+    def __init__(self, remapper, reverse=False, defaultValue=0., linearTransform=(1., 0.)):
         """! Build a SharedRemappingMulti1D3D object, to be given to an Exchanger.
 
         @param remapper A Multi1D3DRemapper object performing the projection. It can thus be shared with other instances of
                SharedRemappingMulti1D3D (its initialization will always be done only once).
-        @param reverse Allows the remapper to be shared with an instance of SharedRemappingMulti1D3D performing the reverse exchange
-               (the projection will be done in the reverse direction if reverse is set to True). Direct is multi1D -> 3D, reverse is 3D -> multi1D.
-        @param defaultValue This is the default value to be assigned, during the projection, in the meshes of the target mesh which are not
-               intersected by the source mesh.
-        @param linearTransform Tuple (a,b): apply a linear function to all output fields f such as they become a * f + b. The transformation
-               is applied after the mesh projection.
-        @param outsideCellsScreening If set to True, target cells whose barycentre is outside of source mesh are screen out (defaultValue is assigned
-            to them). It can be useful to screen out cells that are in contact with the source mesh, but that should not be intersected by it.
-            On the other hand, it will screen out cells actually intersected if their barycenter is outside of source mesh! Be careful with this
-            option.
+        @param reverse see SharedRemapping. Direct is multi1D -> 3D, reverse is 3D -> multi1D.
+        @param defaultValue see SharedRemapping.
+        @param linearTransform see SharedRemapping.
         """
-        self._remapper = remapper
-        self._isReverse = reverse
-        self._defaultValue = defaultValue
-        self._linearTransform = linearTransform
-        self._outsideCellsScreening = outsideCellsScreening
-
-    def initialize(self, fieldsToGet, fieldsToSet, valuesToGet):
-        """! INTERNAL """
-        if not self._remapper.isInit:
-            if self._isReverse:
-                self._remapper.initialize(fieldsToSet[0].getMesh(), fieldsToGet[0].getMesh())
-            else:
-                self._remapper.initialize(fieldsToGet[0].getMesh(), fieldsToSet[0].getMesh())
+        SharedRemapping.__init__(self, remapper, reverse, defaultValue, linearTransform)
 
     def __call__(self, fieldsToGet, fieldsToSet, valuesToGet):
         """! Project the input fields one by one before returning them as outputs, in the same order. """
-        self.initialize(fieldsToGet, fieldsToSet, valuesToGet)
-        resu = []
+        #self.initialize(fieldsToGet, fieldsToSet, valuesToGet)
+        if not self._remapper.isInnerFieldBuilt:
+            self._remapper.buildInnerField(fieldsToSet[0].getMesh() if self._isReverse else fieldsToGet[0].getMesh())
         if self._isReverse:
-            for field3D in fieldsToGet:
-                resu += self._remapper.build1DFields(field3D, self._defaultValue, self._outsideCellsScreening)
-        else:
-            indexFirst = 0
-            while indexFirst + len(self._remapper._indexTable) <= len(fieldsToGet):
-                fields1D = [fieldsToGet[indexFirst + k] for k in range(len(self._remapper._indexTable))]
-                resu += [self._remapper.build3DField(fields1D, self._defaultValue, self._outsideCellsScreening)]
-                indexFirst += len(self._remapper._indexTable)
-        if self._linearTransform != (1., 0.):
-            for med in resu:
-                med.applyLin(*(self._linearTransform))
-        return resu, valuesToGet
+            outputFields, outputValues = SharedRemapping.__call__(self, fieldsToGet, [self._remapper.getInnerField()]*len(fieldsToGet), valuesToGet)
+            resu = []
+            for field3D in outputFields:
+                resu += self._remapper.build1DFields(field3D)
+            return resu, outputValues
+        indexFirst = 0
+        intermediate3DField = []
+        while indexFirst + len(self._remapper._indexTable) <= len(fieldsToGet):
+            fields1D = [fieldsToGet[indexFirst + k] for k in range(len(self._remapper._indexTable))]
+            intermediate3DField.append(self._remapper.build3DField(fields1D))
+            indexFirst += len(self._remapper._indexTable)
+        return SharedRemapping.__call__(self, intermediate3DField, fieldsToSet, valuesToGet)
