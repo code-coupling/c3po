c8d32d85b7775a18e27d5c09a8143a76f086271c
PATRICOT Cyril <cpatricot@is233268.intra.cea.fr>
2023-03-03 11:30:10 +0100

    Premiere version de MPIExchanger compatible avec l'utilisation d'un MPIExchangeMethod.

--- sources/c3po/mpi/MPIExchanger.py
+++ sources/c3po/mpi/MPIExchanger.py
@@ -8,12 +8,15 @@
 # 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-""" Contain the classes MPIExchanger and MPIShortcutToData, MPIShortcutToData is for internal use only. """
+""" Contain the classes MPIExchanger and MPIShortcutToData. MPIShortcutToData is for internal use only. """
 from __future__ import print_function, division
+from mpi4py import MPI as mpi
 
 from c3po.LocalExchanger import LocalExchanger, ShortcutToField, ShortcutToValue
 from c3po.CollaborativeExchanger import CollaborativeExchanger
+from c3po.mpi.MPIRemote import MPIRemote
 from c3po.mpi.MPIRemoteProcess import MPIRemoteProcess
+from c3po.mpi.MPIRemoteProcesses import MPIRemoteProcesses
 from c3po.mpi.MPICollectiveProcess import MPICollectiveProcess
 from c3po.mpi.MPISender import MPIFieldSender, MPIFileFieldSender, MPIValueSender
 from c3po.mpi.MPIRecipient import MPIFieldRecipient, MPIFileFieldRecipient, MPIValueRecipient
@@ -47,42 +50,69 @@ class MPIShortcutToData(object):
 class MPIExchanger(LocalExchanger):
     """! MPIExchanger is the MPI version of c3po.LocalExchanger.LocalExchanger.
 
-    The class takes in charge data exchanges between MPI processes (in the case where each code exposes its data on a single
-    MPI process). It manages the MPI exchanges before managing the local exchanges with the mother class.
-
-    Can replace, without impact, an c3po.LocalExchanger.LocalExchanger for a calculation on a single process, if the MPI environment is available.
+    MPIExchanger can replace, without impact, a c3po.LocalExchanger.LocalExchanger for a calculation on a single process, if
+    the MPI environment is available.
     """
 
-    def __init__(self, method, fieldsToGet, fieldsToSet, valuesToGet=[], valuesToSet=[], exchangeWithFiles=False):
+    def __init__(self, method, fieldsToGet, fieldsToSet, valuesToGet=[], valuesToSet=[], exchangeWithFiles=False, mpiComm=None):
         """! Build a MPIExchanger object.
 
-        Has the same form as LocalExchanger.__init__() but can also contain MPIRemoteProcess and MPICollectiveProcess
-        (and therefore MPICollectiveDataManager) objects.
+        The constructor has the same form than LocalExchanger.__init__() with two additional optionnal parameters:
+        exchangeWithFiles and mpiComm.
 
-        MPIExchanger must be built in the same way for all the processes involved in the exchanges. Likewise, the exchange() method
-        must be called at the same time by all processes.
+        MPIExchanger must be built in the same way for all the processes involved in the exchanges. Likewise, the exchange()
+        method must be called at the same time by all processes.
 
-        It is assumed that an object is either held by a single process (and of the form MPIRemoteProcess in others), or collective
-        (MPICollectiveProcess).
+        The first parameter, method, can be either a c3po.mpi.MPIExchangeMethods.MPIExchangeMethod.MPIExchangeMethod or not:
 
-        When there is an MPICollectiveProcess on the get side, all the processes of the communicator of this object must be
-        involved in the exchanges.
+            - In the first case, the exchange method has to deal with data exchanges between MPI processes (in addition to
+            local data processing such as unit change). MPIRemoteProcess and MPIRemoteProcesses are accepted, but not
+            MPICollectiveProcess.
 
-        See LocalExchanger.__init__() for the documentation of other parameters than exchangeWithFiles.
+            - In the second case, these exchanges between MPI processes are done by MPIExchanger. The class manages MPI
+            exchanges and then uses the local exchange method with the mother class.
 
-        when exchangeWithFiles is set to True, exchanged MEDField are written on files and read by the recipient process(es).
-        Only basic data (such as the file path) are exchanged via MPI.
+            In this case, it is assumed that an object is either held by a single process (and is replace by a
+            MPIRemoteProcess in others), or collectively (MPICollectiveProcess). When there is an MPICollectiveProcess on
+            the set side, all the processes of the communicator of this object must be involved in the exchanges.
+            MPIRemoteProcesses are not accepted.
+
+        @param exchangeWithFiles (bool) Only available with an exchange method that is not of type
+            c3po.mpi.MPIExchangeMethods.MPIExchangeMethod.MPIExchangeMethodwhen! When exchangeWithFiles is set to True, exchanged
+            MEDField are written on files and read by the recipient process(es). Only basic data (such as the file path) are
+            exchanged via MPI.
+        @param mpiComm Only available with an exchange method that is of type
+            c3po.mpi.MPIExchangeMethods.MPIExchangeMethod.MPIExchangeMethodwhen! If not None, it indicated to the exchange method
+            to use this communicator for data exchange. Otherwise, all MPIRemoteProcess and MPIRemoteProcesses must use the same
+            communicator, which is then used by the exchange method.
+
+        See LocalExchanger.__init__() for the documentation of the other parameters.
         """
+
+        self._subExchangers = None
+        self._dataNeeded = True
+        self._mpiExchanges = []
+
         fieldsToGet = self._expandInputList(fieldsToGet)
         fieldsToSet = self._expandInputList(fieldsToSet)
         valuesToGet = self._expandInputList(valuesToGet)
         valuesToSet = self._expandInputList(valuesToSet)
 
+        if isinstance(method, MPIExchangeMethod):
+            if exchangeWithFiles:
+                raise ValueError("MPIExchanger.__init__: exchangeWithFiles option cannot be activated when using an exchange method of type MPIExchangeMethod.")
+            self._initWithMPIExchangeMethod(method, fieldsToGet, fieldsToSet, valuesToGet, valuesToSet, mpiComm)
+        else:
+            if mpiComm is not None:
+                raise ValueError("MPIExchanger.__init__: mpiComm cannot be specified when using an exchange method that is not of type MPIExchangeMethod.")
+            self._initWithLocalExchangeMethod(method, fieldsToGet, fieldsToSet, valuesToGet, valuesToSet, exchangeWithFiles, mpiComm)
+
+    def _initWithLocalExchangeMethod(self, method, fieldsToGet, fieldsToSet, valuesToGet=[], valuesToSet=[], exchangeWithFiles=False):
+        """! INTERNAL """
         dividedInputs = self._divideInputsAccordingToPatterns(method, fieldsToGet, fieldsToSet, valuesToGet, valuesToSet)
 
-        self._subExchangers = None
         self._dataNeeded = False
-        self._isCollective = False
+        self._mpiExchanges = []
 
         if len(dividedInputs) > 1:
             self._subExchangers = []
@@ -93,6 +123,12 @@ class MPIExchanger(LocalExchanger):
             LocalExchanger.__init__(self, method, fieldsToGet, fieldsToSet, valuesToGet, valuesToSet)
 
             destinations = []
+            isCollective = False
+
+            # On verifie qu'il n'y a pas de MPIRemoteProcesses (non supportes)
+            for obj in [data[0] for arg in [fieldsToGet, fieldsToSet, valuesToGet, valuesToSet] for data in arg]:
+                if isinstance(obj, MPIRemoteProcesses):
+                    raise Exception("MPIExchanger.__init__: MPIRemoteProcesses is not allowed with an exchange method that is not of type MPIExchangeMethod.")
 
             # On regarde si on a une communication collective dans les set, auquel cas toutes les communications deviennent collectives
             for i in range(len(fieldsToSet)):
@@ -100,19 +136,19 @@ class MPIExchanger(LocalExchanger):
                 if isinstance(toSet, MPICollectiveProcess):
                     destinations.append(toSet)
                     self._dataNeeded = True
-                    self._isCollective = True
+                    isCollective = True
                     break
-            if not self._isCollective:
+            if not isCollective:
                 for i in range(len(valuesToSet)):
                     toSet = valuesToSet[i][0]
                     if isinstance(toSet, MPICollectiveProcess):
                         destinations.append(toSet)
                         self._dataNeeded = True
-                        self._isCollective = True
+                        isCollective = True
                         break
 
             # Si on n'a pas de communications collectives, on cherche les destinataires des envois du rank local
-            if not self._isCollective:
+            if not isCollective:
                 for i in range(len(fieldsToSet)):
                     toSet = fieldsToSet[i][0]
                     if isinstance(toSet, MPIRemoteProcess) and toSet not in destinations:
@@ -127,7 +163,6 @@ class MPIExchanger(LocalExchanger):
                         self._dataNeeded = True
 
             # On cree enfin les objets Sender et Recipient
-            self._mpiExchanges = []
             for i in range(len(fieldsToGet)):
                 toGet = fieldsToGet[i][0]
                 if not isinstance(toGet, MPICollectiveProcess):
@@ -136,7 +171,7 @@ class MPIExchanger(LocalExchanger):
                         fieldSender = MPIFileFieldSender(destinations, ShortcutToField(*fieldsToGet[i]), self._fieldsToGet[i], False) if exchangeWithFiles else MPIFieldSender(destinations, ShortcutToField(*fieldsToGet[i]), self._fieldsToGet[i], False)
                         self._mpiExchanges.append(fieldSender)
                     elif self._dataNeeded:
-                        fieldRecipient = MPIFileFieldRecipient(toGet, self._fieldsToGet[i], self._isCollective, False) if exchangeWithFiles else MPIFieldRecipient(toGet, self._fieldsToGet[i], self._isCollective, False)
+                        fieldRecipient = MPIFileFieldRecipient(toGet, self._fieldsToGet[i], isCollective, False) if exchangeWithFiles else MPIFieldRecipient(toGet, self._fieldsToGet[i], isCollective, False)
                         self._mpiExchanges.append(fieldRecipient)
             for i in range(len(fieldsToSet)):
                 toSet = fieldsToSet[i][0]
@@ -146,7 +181,7 @@ class MPIExchanger(LocalExchanger):
                         fieldSender = MPIFileFieldSender(destinations, ShortcutToField(*fieldsToSet[i]), self._fieldsToSet[i], True) if exchangeWithFiles else MPIFieldSender(destinations, ShortcutToField(*fieldsToSet[i]), self._fieldsToSet[i], True)
                         self._mpiExchanges.append(fieldSender)
                     elif self._dataNeeded:
-                        fieldRecipient = MPIFileFieldRecipient(toSet, self._fieldsToSet[i], self._isCollective, True) if exchangeWithFiles else MPIFieldRecipient(toSet, self._fieldsToSet[i], self._isCollective, True)
+                        fieldRecipient = MPIFileFieldRecipient(toSet, self._fieldsToSet[i], isCollective, True) if exchangeWithFiles else MPIFieldRecipient(toSet, self._fieldsToSet[i], isCollective, True)
                         self._mpiExchanges.append(fieldRecipient)
             for i in range(len(valuesToGet)):
                 toGet = valuesToGet[i][0]
@@ -155,7 +190,58 @@ class MPIExchanger(LocalExchanger):
                     if not isinstance(toGet, MPIRemoteProcess):
                         self._mpiExchanges.append(MPIValueSender(destinations, ShortcutToValue(*valuesToGet[i]), self._valuesToGet[i]))
                     elif self._dataNeeded:
-                        self._mpiExchanges.append(MPIValueRecipient(toGet, self._valuesToGet[i], self._isCollective))
+                        self._mpiExchanges.append(MPIValueRecipient(toGet, self._valuesToGet[i], isCollective))
+
+    def _initWithMPIExchangeMethod(self, method, fieldsToGet, fieldsToSet, valuesToGet=[], valuesToSet=[], mpiComm=None):
+        """! INTERNAL """
+        ranksToGet = []
+        ranksToSet = []
+        ExchangerMPIComm = mpiComm
+
+        if mpiComm is None:
+            for obj in [data[0] for arg in [fieldsToGet, fieldsToSet, valuesToGet, valuesToSet] for data in arg]:
+                if isinstance(obj, MPIRemote):
+                    if obj.mpiComm == mpi.COMM_NULL:
+                        raise Exception("MPIExchanger.__init__: All distant processes must be part of the communicator (mpi.COMM_NULL found).")
+                    if ExchangerMPIComm is not None and ExchangerMPIComm != obj.mpiComm:
+                        raise Exception("MPIExchanger.__init__: All distant processes must used the same MPI communicator.")
+                    ExchangerMPIComm = obj.mpiComm
+        if ExchangerMPIComm is None:
+            raise Exception("MPIExchanger.__init__: No MPI communicator found.")
+
+        def _readRanks(dataAccessor):
+            """! INTERNAL """
+            ranks = []
+            if isinstance(dataAccessor, MPIRemoteProcess):
+                ranks = mpi.Group.Translate_ranks(dataAccessor.mpiComm.Get_group(), [dataAccessor.rank], ExchangerMPIComm.Get_group())
+            elif isinstance(dataAccessor, MPIRemoteProcesses):
+                ranks = mpi.Group.Translate_ranks(dataAccessor.mpiComm.Get_group(), dataAccessor.ranks, ExchangerMPIComm.Get_group())
+            else:
+                try:
+                    intersecGroup = mpi.Group.Intersection(dataAccessor.getMPIComm().Get_group(), ExchangerMPIComm.Get_group())
+                    ranks = mpi.Group.Translate_ranks(intersecGroup, list(range(intersecGroup.Get_size())), ExchangerMPIComm.Get_group())
+                except NotImplementedError:
+                    ranks = [ExchangerMPIComm.Get_rank()]
+            return ranks
+
+        def _initObjectList(initialList, ranks):
+            """! INTERNAL """
+            newList = []
+            for elem in initialList:
+                newRanks = _readRanks(elem[0])
+                ranks += list(set(newRanks) - set(ranks))
+                if not isinstance(elem[0], MPIRemote):
+                    newList.append(elem)
+            return newList
+
+        localFieldsToGet = _initObjectList(fieldsToGet, ranksToGet)
+        localFieldsToSet = _initObjectList(fieldsToSet, ranksToSet)
+        localValuesToGet = _initObjectList(valuesToGet, ranksToGet)
+        localValuesToSet = _initObjectList(valuesToSet, ranksToSet)
+
+        LocalExchanger.__init__(self, method, localFieldsToGet, localFieldsToSet, localValuesToGet, localValuesToSet)
+
+        method.setRanks(ranksToGet, ranksToSet, ExchangerMPIComm)
 
     def exchange(self):
         """! Trigger the exchange of data.
