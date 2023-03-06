bdf04f9278b21123772132c948baf1049bdd4d7c
PATRICOT Cyril <cpatricot@is233268.intra.cea.fr>
2023-03-06 15:47:38 +0100

    Ajout / modifs des __init__.py.

--- sources/c3po/mpi/mpiExchangeMethods/__init__.py
+++ sources/c3po/mpi/mpiExchangeMethods/__init__.py
@@ -7,22 +7,3 @@
 # 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 # 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-""" Import user MPI C3PO objects. """
-
-from .MPIRemote import MPIRemote
-from .MPIRemoteProcess import MPIRemoteProcess
-from .MPIRemoteProcesses import MPIRemoteProcesses
-from .MPICollectiveProcess import MPICollectiveProcess
-from .MPICollectiveDataManager import MPICollectiveDataManager
-from .MPICollaborativeDataManager import MPICollaborativeDataManager
-from .MPICollaborativePhysicsDriver import MPICollaborativePhysicsDriver
-from .MPIExchanger import MPIExchanger
-from .MPICoupler import MPICoupler
-from .MPIMasterPhysicsDriver import MPIMasterPhysicsDriver
-from .MPIMasterDataManager import MPIMasterDataManager
-from .MPIMasterExchanger import MPIMasterExchanger
-from .MPIWorker import MPIWorker
-from .mpiExchangeMethods.MPIExchangeMethod import MPIExchangeMethod
-from .mpiExchangeMethods.MPISharedRemapping import MPISharedRemapping, MPIRemapper
-from .mpiExchangeMethods.MPISharedRemappingMulti1D3D import MPISharedRemappingMulti1D3D, MPIMulti1D3DRemapper
